3200dbfa579087ab21ab3a8066a9f48279921af2
ChinaHackers <93428739@qq.com>
2018-01-21 22:14:37 +0800

    init project.

---
+++ Wallpaper/Wallpaper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wallpaper
+//
+//  Created by Liu Chuan on 2017/7/11.
+//  Copyright © 2017年 LC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
